04ce7c4c236588e6e4dfede1151afc8c5ccd29bb
bituzin <bituzin2@gmail.com>
2026-01-16 22:28:44 +0100

    Enable Interact popup for Sepolia contracts in MyDeployments.

--- src/components/MyDeployments.jsx
+++ src/components/MyDeployments.jsx
@@ -218,7 +218,7 @@ export const MyDeployments = ({ theme, deployments, isConnected, openModal, netw
                     </div>
 
                     {/* Interact button only for Celo network */}
-                    {deployment.network === 'Celo' && (
+                    {(deployment.network === 'Celo' || deployment.network === 'Sepolia') && (
                       <button
                         style={{
                           marginTop: 10,
