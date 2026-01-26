57d7b2d68069eb6ccd0a09c79a6889e2ce6b210a
bituzin <bituzin2@gmail.com>
2026-01-26 23:27:47 +0100

    UI: align steps 2-5 with step 1 on home page

--- src/App.jsx
+++ src/App.jsx
@@ -360,11 +360,11 @@ function App() {
                   <br />
                   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: 600, margin: '12px 0', width: '100%' }}>
                     <div style={{ width: 220, textAlign: 'center' }}>
-                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0' }}>1. Connect</span>
-                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0' }}>2. Check</span>
-                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0' }}>3. Read</span>
-                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0' }}>4. Deploy</span>
-                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0' }}>5. Interact</span>
+                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 18 }}>1. Connect</span>
+                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 18 }}>2. Check</span>
+                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 18 }}>3. Read</span>
+                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 18 }}>4. Deploy</span>
+                      <span style={{ display: 'block', whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 18 }}>5. Interact</span>
                     </div>
                   </div>
                   <br />
