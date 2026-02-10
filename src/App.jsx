37177e00d550f50f069dd743b7eccf147c92e673
bituzin <bituzin2@gmail.com>
2026-02-10 23:15:06 +0100

    Content: add '* read How it works' above contracts info on Home page

--- src/App.jsx
+++ src/App.jsx
@@ -326,7 +326,10 @@ function App() {
                   Deploy your own contract in seconds!
                 </span>
                 <span style={{ fontSize: '0.74em', fontStyle: 'italic', color: theme.textSecondary, marginTop: 24, display: 'block', fontFamily: 'Georgia, Times, Times New Roman, serif' }}>
-                  read <a href="/how" style={{ color: theme.textPrimary, textDecoration: 'underline', fontWeight: 'bold' }}>How it works</a>.
+                  * read <a href="/how" style={{ color: theme.textPrimary, textDecoration: 'underline', fontWeight: 'bold' }}>How it works</a>.
+                </span>
+                <span style={{ fontSize: '0.74em', fontStyle: 'italic', color: theme.textSecondary, marginTop: 8, display: 'block', fontFamily: 'Georgia, Times, Times New Roman, serif' }}>
+                  *currently 4 contracts available, more coming soon.
                 </span>
               </div>
             )} />
