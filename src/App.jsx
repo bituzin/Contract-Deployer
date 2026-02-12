77d51fb1149201cfa686a180a4d6c78fad00bac5
bituzin <bituzin2@gmail.com>
2026-02-12 22:32:09 +0100

    Shift bullet list items (Connect, Check, Read, Deploy, Interact) 100px to the right on home page

--- src/App.jsx
+++ src/App.jsx
@@ -370,11 +370,11 @@ function App() {
                       <br />
                       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: 600, margin: '12px 0', width: '100%' }}>
                         <div style={{ width: 220, textAlign: 'left', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', margin: '0 auto' }}>
-                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0' }}>• Connect</span>
-                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0' }}>• Check</span>
-                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0' }}>• Read</span>
-                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0' }}>• Deploy</span>
-                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0' }}>• Interact</span>
+                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 100 }}>• Connect</span>
+                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 100 }}>• Check</span>
+                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 100 }}>• Read</span>
+                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 100 }}>• Deploy</span>
+                          <span style={{ whiteSpace: 'nowrap', margin: '2px 0', paddingLeft: 100 }}>• Interact</span>
                         </div>
                       </div>
                       <br />
